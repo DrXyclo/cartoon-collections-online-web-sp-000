7ccb7e961aa450985ad8f217622ce2fba3c8fea1
Dr. XyClo <sdcarmody@gmail.com>
2019-01-03 17:47:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -58,8 +58,8 @@ cheese_types = ["cheddar", "gouda", "camembert"]
 def find_the_cheese(cheddar_cheese, cheese_types)
   
   cheddar_cheese.detect { |array_objects| cheese_types.include?(array_objects)}
-  binding.pry
-  puts "true" 
+  # binding.pry
+ 
 end 
   
   # cheddar_cheese.collect |array_objects|
